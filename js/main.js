7279cc18d8ff76e6549087e5bfe449ca78201d41
Tim Buckley <ninjay@gmail.com>
2013-12-30 22:21:11 -0500

    add content, basic javascript for tab switching

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+	$("#nav-list a").on("click", function(event) {
+		$("#content div").each(function() {
+			$(this).removeClass("active");
+		});
+		$("#content #" + this.id).toggleClass("active");
+	});
+});
