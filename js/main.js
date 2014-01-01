839f5b0dc6b0a4fcbc8dcdadbc4491ece846a8e3
Tim Buckley <ninjay@gmail.com>
2014-01-01 16:36:53 -0500

    js typo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(function () {
-	$("#nav-list a").on("click", function(event) {
-		$("#content div").each(function() {
-			$(this).removeClass("active");
-		});
-		$("#content #" + this.id).toggleClass("active");
-	});
+$(function() {
+  $("#nav-list a").on("click", function(event) {
+    $("#content div").each(function() {
+      $(this).removeClass("active");
+    });
+    $("#content #" + this.id).toggleClass("active");
+  });
 });
